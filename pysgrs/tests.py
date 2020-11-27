da47da8c42b2c72dcf86dbd3c1968020978aed9a
jlandercy <jeanlandercy@live.com>
2020-11-27 10:57:52 +0100

    Starting pysgrs w/ PyCharm

--- pysgrs/tests.py
+++ pysgrs/tests.py
@@ -58,7 +58,7 @@ class Alphabets(unittest.TestCase):
 class Cyphers(unittest.TestCase):
 
     def setUp(self):
-        self.identity = base.Caesar()
+        self.identity = base.Caesar(offset=0)
         self.ceasar = base.Caesar(offset=3)
 
     def test_cypher_illegalchar(self):
