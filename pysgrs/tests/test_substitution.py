1940ea617b263c8b9fd014581cc7aeb406c0ce7d
jlandercy <jeanlandercy@live.com>
2020-11-27 16:45:46 +0100

    Added another test

--- pysgrs/tests/test_substitution.py
+++ pysgrs/tests/test_substitution.py
@@ -17,6 +17,22 @@ class TestIdentityCypher(TestCypher, unittest.TestCase):
 
 class TestRotationCypher(TestCypher, unittest.TestCase):
 
+    cypher = RotationCypher(offset=7)
+    cyphers = [
+        "HIJKLMNOPQRSTUVWXYZABCDEFG"
+    ]
+
+
+class TestNegativeRotationCypher(TestCypher, unittest.TestCase):
+
+    cypher = RotationCypher(offset=-7)
+    cyphers = [
+        "TUVWXYZABCDEFGHIJKLMNOPQRS"
+    ]
+
+
+class TestCaesarCypher(TestCypher, unittest.TestCase):
+
     cypher = CaesarCypher()
     cyphers = [
         "DEFGHIJKLMNOPQRSTUVWXYZABC"
@@ -29,31 +45,6 @@ class TestReversedCypher(TestCypher, unittest.TestCase):
     cyphers = [
         "ZYXWVUTSRQPONMLKJIHGFEDCBA"
     ]
-
-
-class OldTestRotationCypher(unittest.TestCase):
-
-    def setUp(self):
-        self.identity = RotationCypher(offset=0)
-        self.cypher = CaesarCypher()
-
-    def test_cypher_illegalchar(self):
-        with self.assertRaises(errors.IllegalCharacter):
-            self.cypher.cypher("CaVECANEM")
-        self.assertEqual(self.cypher.cypher("CaVE CANEM", strict=False, quite=True), "FDYH FDQHP")
-
-    def test_cypher_caseinsensitive(self):
-            self.assertEqual(self.cypher.cypher("CaVECANEM", strict=False), "FDYHFDQHP")
-
-    def test_IdentityCypher(self):
-        self.assertEqual(self.identity.alphabet.alphabet,
-                         self.identity.decypher(self.identity.cypher(self.identity.alphabet.alphabet)))
-        self.assertEqual(self.identity.cypher("CAVECANEM"), "CAVECANEM")
-
-    def test_Cypher(self):
-        self.assertEqual(self.cypher.alphabet.alphabet,
-                         self.cypher.decypher(self.cypher.cypher(self.cypher.alphabet.alphabet)))
-        self.assertEqual(self.cypher.cypher("CAVECANEM"), "FDYHFDQHP")
 
 
 def main():
