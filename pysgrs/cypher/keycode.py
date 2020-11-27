0e0dd1f916fd2d980a0c5f774ddd1cbbe3653a60
jlandercy <jeanlandercy@live.com>
2020-11-27 14:24:33 +0100

    Added simple key code

--- pysgrs/cypher/keycode.py
+++ pysgrs/cypher/keycode.py
@@ -10,10 +10,6 @@ class KeyCode(GenericStreamCypher):
     def __init__(self, key, alphabet=None):
         super().__init__(alphabet=alphabet, key=key)
 
-    @property
-    def key(self):
-        return self._key
-
     def _cypher(self, c, k):
         return (self.alphabet.index(c) + self.alphabet.index(self.key[k % self.keysize])) % self.alphabet.size
 
