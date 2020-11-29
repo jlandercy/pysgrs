5bc3c1f4b269441e64ad0b2f9f82be0ceeacd6c9
jlandercy <jeanlandercy@live.com>
2020-11-29 12:52:52 +0100

    Refactored Cypher inheritance

--- pysgrs/cyphers/shape.py
+++ pysgrs/cyphers/shape.py
@@ -1,4 +1,8 @@
 import sys
+import itertools
+
+import numpy as np
+import pandas as pd
 
 from pysgrs.interfaces.cypher import GenericShapeCypher
 from pysgrs.settings import settings
@@ -9,8 +13,19 @@ class TranspositionCypher(GenericShapeCypher):
     def __init__(self, shape=None):
         super().__init__(shape=shape)
 
-    def cypher(self, s):
-        pass
+    def shaper(self, n):
+        m = np.sqrt(n)
+        m0 = int(np.floor(m))
+        m1 = int(np.ceil(m))
+        df = pd.DataFrame(itertools.product([m0, m1], [m0, m1]), columns=["nrow", "ncol"])
+        df["size"] = df["nrow"]*df["ncol"]
+        df["padding"] = df["size"] - n
+        return df
+
+    def cypher(self, s, shape=None):
+        shape = shape or self.shape
+        n = len(s)
+        print(self.shaper(n))
 
     def decypher(self, s):
         pass
--- pysgrs/tests/test_shape.py
+++ pysgrs/tests/test_shape.py
@@ -10,7 +10,7 @@ class TestTranspositionCypher(TestStreamCypher, unittest.TestCase):
     cypher = TranspositionCypher()
     cyphers = [
         "ACEDFHGIKJLNMOQPRTSUWVXZYA"
-    ]
+   ]
 
 
 def main():
