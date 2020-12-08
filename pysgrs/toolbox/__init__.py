d3768834490f27dd76d78e74304aa6f82a07c2e9
jlandercy <jeanlandercy@live.com>
2020-12-08 20:49:40 +0100

    Starting Hill Climbing and Genetic Alpgorithm to solve Vigenere and Subsituation Cipher

--- pysgrs/toolbox/__init__.py
+++ pysgrs/toolbox/__init__.py
@@ -2,3 +2,4 @@ from pysgrs.toolbox.arithmetic import *
 from pysgrs.toolbox.formats import *
 from pysgrs.toolbox.helpers import *
 from pysgrs.toolbox.ngrams import *
+from pysgrs.toolbox.algorithms import *
---
+++ pysgrs/toolbox/algorithms.py
@@ -0,0 +1,47 @@
+import sys
+
+from pysgrs.settings import settings
+
+
+class HillClimbingAlgorithm:
+    pass
+
+
+class GeneticAlgorithm:
+    """
+    https://towardsdatascience.com/introduction-to-genetic-algorithms-including-example-code-e396e98d8bf3
+    START
+    Generate the initial population
+    Compute fitness
+    REPEAT
+        Selection
+        Crossover
+        Mutation
+        Compute fitness
+    UNTIL population has converged
+    STOP
+    """
+
+    def __init__(self):
+        pass
+
+    def select(self):
+        pass
+
+    def crossover(self):
+        pass
+
+    def mutate(self):
+        pass
+
+    def fitness(self):
+        pass
+
+
+def main():
+    settings.logger.warning("Hello world!")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
