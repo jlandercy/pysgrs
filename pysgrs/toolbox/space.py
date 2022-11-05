a38c0402bb68256db809ce84b7fb36dfea8c03c4
jeanlandercy <jeanlandercy@live.com>
2022-11-05 09:55:52 +0100

    Adding Parameters Space

--- pysgrs/toolbox/space.py
+++ pysgrs/toolbox/space.py
@@ -24,10 +24,23 @@ class ParameterSpace:
         return count
 
 
+dummy_space = ParameterSpace(
+    text=[
+        "LA VIE EST UNE ETRANGE ENTREPRISE, PLEINE DE RISQUES ET SANS BUT REVELE.",
+        "LA CRYPTOGRAPHIE EST UNE SUPERBE DISCIPLINE ET EGALEMENT LE REFLET DE NOS LIMITES.",
+        "JE N'AI D'YEUX QUE POUR TOI, L'AMOUR REND AVEUGLE N'EST-CE PAS ?",
+        "LA TELEVISION REND BETE ET MECHANT SURTOUT LES EMISSIONS FAITES PAR DES CONS POUR LES CONS.",
+        "JULES N'A PAS RECU DE CEASAR CETTE ANNEE, PERSONNE NE SAIT SI ON LE REVERA A CANNES."
+    ],
+    seed=[123456789],
+    key=["A", "AB", "ABC", "ABCD", "ABCDE"],
+    population_size=[50, 100, 150, 200],
+    threshold=[0.5],
+    weights=[[0.6, 0.3, 0.1]]
+)
+
 if __name__ == "__main__":
 
-    space = ParameterSpace(a=[1, 2, 3], b=["a", "b"])
-    for p in space.generate():
+    for p in dummy_space.generate():
         print(p)
-
-    print(space.size())
+    print(dummy_space.size())
