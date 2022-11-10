756215b4ae9279815ed8d2a7d9206e2236d97fb9
jeanlandercy <jeanlandercy@live.com>
2022-11-10 14:40:02 +0100

    Adapted challenge

--- pysgrs/tests/test_python_challenge.py
+++ pysgrs/tests/test_python_challenge.py
@@ -12,89 +12,109 @@ from pysgrs.tests.test_breakers_vigenere import GenericVigenereBreakerTest, Basi
 class TestBreaker(BasicVigenereGeneticAlgorithmBreaker, unittest.TestCase):
 
     challenges = [
+        # {
+        #     "cipher_text":
+        #         """
+        #         YIGNJ GWETR GMATB WSRAC RXNBF BVXNY LQQPG XKGWK LHRWC ABFSL
+        #         BECCE PRWRR UGAHQ ESKHW GKFNK GOEIZ SURME IEHMN ZXAGF WXZRR
+        #         HYMWF VPRAQ QNHWE HINOG WHFZL XHVSR BSIXT YSAFS ZQIAQ SIXVG
+        #         ZQQJB VANBK CGIHL IXXEI G
+        #         """,
+        #     "halt_on_exact_key": "NEONICOTINOIDE",
+        #     "key_sizes": [14],
+        #     "cipher_text_id": "jboulanger-1"
+        # },
+        # {
+        #     "cipher_text":
+        #         """
+        #         YIGNJ GWETR GMATB WSRAC RXNBF BVXNY LQQPG XKGWK LHRWC ABFSL
+        #         BECCE PRWRR UGAHQ ESKHW GKFNK GOEIZ SURME IEHMN ZXAGF WXZRR
+        #         HYMWF VPRAQ QNHWE HINOG WHFZL XHVSR BSIXT YSAFS ZQIAQ SIXVG
+        #         ZQQJB VANBK CGIHL IXXEI GPMUH ZZNQM DGRPO DCGZE MFFMW VBYJR
+        #         VVZXK USULR QIZNZ WQAMY SALRF IQGQE WWMFB MWYRR HCIUZ XANPM
+        #         LPYIG FCTZX KBIXP EVWSY TGGLW HTNUI AXRRT GGBWA GKHVR FFNTG
+        #         GXBFC VWMAG OCIDZ XAQSJ XXVRS EMVRT AFCKL IEPSC ITTNU QIVHJ
+        #         YIIEI XSVTR DWOPR RSGTG BXKGO ZLPRB WFBGI GXUSV RQRRS NXRSE
+        #         MFMVG VBQSQ MHTHV QFMPI AXRRA ECEWA WMVHN FSVTN SLKRZ THWPM
+        #         GRDQZ TBVZQ VIAXH BCVGB UCZMP IAXSY TGGJC VHBHR GPOEC EVXMG
+        #         BMUII MSAVG BMXYI A
+        #         """,
+        #     "halt_on_exact_key": "NEONICOTINOIDE",
+        #     "key_sizes": [14],
+        #     "cipher_text_id": "jboulanger-2"
+        # },
+        # {
+        #     "cipher_text":
+        #         """
+        #         XVBDU IQEQA NJQFP TFCMC BERUS CICGT CFNLB VPNJC GXOLV TGXNK
+        #         IBVIA JIJDN LVUIA OLVSJ NFUWI WTQIF XLRCK IWGFQ FFAEA BTJVY
+        #         ZSXTU IGAIA SIDPI JWNBC HWQJP IKIEI PNQLG CUERH CFFDV GJNFC
+        #         ECWQL ZSXTA IWQGG XINPI JQVQJ VQOLR IEYVW IFLVK PLFZL VCHVD
+        #         AKIFV LLOAV TSEAZ FFIWF XVSJN ZTLWB GWWMH DZAII FHOWF CAAIF
+        #         IWFVC HDUIY NWWNX RGJRU PNQHH GWAHP RZMQF GRVLD UIABV CAQWM
+        #         HACTH VGGRC KAADW KB
+        #         """,
+        #     "halt_on_exact_key": "DISPARITION",
+        #     "key_sizes": [11],
+        #     "cipher_text_id": "jboulanger-3"
+        # },
+        # {
+        #     "cipher_text":
+        #         """
+        #         Jv qpbl, wpv bpr gww'cj axidg kfxl ewtcb qxpqpu
+        #         Qd eqdg r jxym gql uxet zgdcbumz
+        #         Op dpmpmt kmaw um yycc B eiu ashm i kjzjs
+        #         Mpmuv ygx bpg egvaba vyyi gmdgi bxx
+        #         Ug hrrwxz bqcb bx
+        #         """,
+        #     "halt_on_exact_key": "CRYPTII",
+        #     "key_sizes": [7],
+        #     "cipher_text_id": "tganty-1"
+        # },
+        # {
+        #     "cipher_text":
+        #         """
+        #         S'égyqvfé à bro qzcjl (Hnjlpodggfjg lkmdsnayj), wm éuvvsbé lcklfnsmvv,
+        #         wkh hc alueatèel zzdsfh cgoeqimszlrk amj hbjh wm lwfepxfqjw ohhhcidasa
+        #         (pp e'mkl oohsyb ims qhrj ymwzdjsd îtwk rh usil ; u'wgg as xieewsèyi
+        #         rclgquicym imw n si gtmk jnhhp bwjfvaszzw wb Njgezsdwr) lx uifk zrh
+        #         férqgfg pôamèimk wh zdbeiyfshzij lm kiq-tge lw do Avymmdds-Tjwyém.
+        #         """,
+        #     "halt_on_exact_key": "HERISSONPOLISSON",
+        #     "key_sizes": [16],
+        #     "cipher_text_id": "jlandercy-1"
+        # },
+        # {
+        #     "cipher_text":
+        #         """
+        #         Re qrti vk Nsrtrw
+        #         Kwh duvlk h'omumj lewk
+        #         Jy eumbj ur dk tfvyyek
+        #         Yb inyek qolbeay
+        #         """,
+        #     "halt_on_exact_key": "GEORGES",
+        #     "key_sizes": [7],
+        #     "cipher_text_id": "cganty-1"
+        # },
+        # {
+        #     "cipher_text": "Auvhrixl mc fzue",
+        #     "halt_on_exact_key": "ABCDEFGHIJKLMNOP",
+        #     "key_sizes": [16],
+        #     "cipher_text_id": "jlandercy-2"
+        # },
         {
-            "cipher_text":
-                """
-                YIGNJ GWETR GMATB WSRAC RXNBF BVXNY LQQPG XKGWK LHRWC ABFSL
-                BECCE PRWRR UGAHQ ESKHW GKFNK GOEIZ SURME IEHMN ZXAGF WXZRR
-                HYMWF VPRAQ QNHWE HINOG WHFZL XHVSR BSIXT YSAFS ZQIAQ SIXVG
-                ZQQJB VANBK CGIHL IXXEI G
-                """,
-            "halt_on_exact_key": "NEONICOTINOIDE",
-            "key_sizes": [14],
-            "cipher_text_id": "jboulanger-1"
-        },
-        {
-            "cipher_text":
-                """
-                YIGNJ GWETR GMATB WSRAC RXNBF BVXNY LQQPG XKGWK LHRWC ABFSL
-                BECCE PRWRR UGAHQ ESKHW GKFNK GOEIZ SURME IEHMN ZXAGF WXZRR
-                HYMWF VPRAQ QNHWE HINOG WHFZL XHVSR BSIXT YSAFS ZQIAQ SIXVG
-                ZQQJB VANBK CGIHL IXXEI GPMUH ZZNQM DGRPO DCGZE MFFMW VBYJR
-                VVZXK USULR QIZNZ WQAMY SALRF IQGQE WWMFB MWYRR HCIUZ XANPM
-                LPYIG FCTZX KBIXP EVWSY TGGLW HTNUI AXRRT GGBWA GKHVR FFNTG
-                GXBFC VWMAG OCIDZ XAQSJ XXVRS EMVRT AFCKL IEPSC ITTNU QIVHJ
-                YIIEI XSVTR DWOPR RSGTG BXKGO ZLPRB WFBGI GXUSV RQRRS NXRSE
-                MFMVG VBQSQ MHTHV QFMPI AXRRA ECEWA WMVHN FSVTN SLKRZ THWPM
-                GRDQZ TBVZQ VIAXH BCVGB UCZMP IAXSY TGGJC VHBHR GPOEC EVXMG
-                BMUII MSAVG BMXYI A
-                """,
-            "halt_on_exact_key": "NEONICOTINOIDE",
-            "key_sizes": [14],
-            "cipher_text_id": "jboulanger-2"
-        },
-        {
-            "cipher_text":
-                """
-                XVBDU IQEQA NJQFP TFCMC BERUS CICGT CFNLB VPNJC GXOLV TGXNK
-                IBVIA JIJDN LVUIA OLVSJ NFUWI WTQIF XLRCK IWGFQ FFAEA BTJVY
-                ZSXTU IGAIA SIDPI JWNBC HWQJP IKIEI PNQLG CUERH CFFDV GJNFC
-                ECWQL ZSXTA IWQGG XINPI JQVQJ VQOLR IEYVW IFLVK PLFZL VCHVD
-                AKIFV LLOAV TSEAZ FFIWF XVSJN ZTLWB GWWMH DZAII FHOWF CAAIF
-                IWFVC HDUIY NWWNX RGJRU PNQHH GWAHP RZMQF GRVLD UIABV CAQWM
-                HACTH VGGRC KAADW KB
-                """,
-            "halt_on_exact_key": "DISPARITION",
-            "key_sizes": [11],
-            "cipher_text_id": "jboulanger-3"
-        },
-        {
-            "cipher_text":
-                """
-                Jv qpbl, wpv bpr gww'cj axidg kfxl ewtcb qxpqpu
-                Qd eqdg r jxym gql uxet zgdcbumz
-                Op dpmpmt kmaw um yycc B eiu ashm i kjzjs
-                Mpmuv ygx bpg egvaba vyyi gmdgi bxx
-                Ug hrrwxz bqcb bx
-                """,
-            "halt_on_exact_key": "CRYPTII",
-            "key_sizes": [7],
-            "cipher_text_id": "tganty-1"
-        },
-        {
-            "cipher_text":
-                """
-                S'égyqvfé à bro qzcjl (Hnjlpodggfjg lkmdsnayj), wm éuvvsbé lcklfnsmvv,
-                wkh hc alueatèel zzdsfh cgoeqimszlrk amj hbjh wm lwfepxfqjw ohhhcidasa
-                (pp e'mkl oohsyb ims qhrj ymwzdjsd îtwk rh usil ; u'wgg as xieewsèyi
-                rclgquicym imw n si gtmk jnhhp bwjfvaszzw wb Njgezsdwr) lx uifk zrh
-                férqgfg pôamèimk wh zdbeiyfshzij lm kiq-tge lw do Avymmdds-Tjwyém.
-                """,
-            "halt_on_exact_key": "HERISSONPOLISSON",
-            "key_sizes": [16],
-            "cipher_text_id": "jlandercy-1"
-        },
-        {
-            "cipher_text":
-                """
-                Re qrti vk Nsrtrw
-                Kwh duvlk h'omumj lewk
-                Jy eumbj ur dk tfvyyek
-                Yb inyek qolbeay
-                """,
-            "halt_on_exact_key": "GEORGES",
-            "key_sizes": [7],
-            "cipher_text_id": "cganty-1"
+            "cipher_text": """
+                Pstv o yhw alvwcrw huaéumpctlg oymj-nzme usyazrggdre lzhw hr rrzxttwp
+                lh ha dtnevr tplukj. Syoi cpwvyzfae à dhw azqjvrf xprprgk, p'Spzfbqhpwp
+                hh Vbrppluxp mv s'Vvewcdppyw hp mmeêheh, qltw fiaxgatuixmpa à qrf mhpènif,
+                wpwl i lbr jscri hn yih pwxqpa uvif-pijdlprk rzpzvg. Ropp di qihvgie hr
+                rzqbdrf iken h'nmxcla ywerroppoyf, eitcdtlvv ksf zwjcsif wx o'hckfrv mydifnrw
+                pu grp. Ptnl sfg aduxmfw à hpz xiérnwmzyw hn à qih pluedqvlg, znqh np wrefwl
+                xrg zhrlnéi do fixn oh wzv cpfr qm géplvgaxtvv cwzlxép.
+            """,
+            "halt_on_exact_key": "LHIRONDELLEDUNEPALDELICHONNIPALENSE",
+            "key_sizes": [35],
+            "cipher_text_id": "jlandercy-3"
         }
     ]
 
@@ -105,8 +125,8 @@ class TestBreaker(BasicVigenereGeneticAlgorithmBreaker, unittest.TestCase):
     ascending = False
 
     parameters_space = toolbox.ParameterSpace(
-        max_steps=[50],
-        seed=[123456, 7890123, 5678901],
+        max_steps=[100],
+        seed=[123456, 7890123, 5678901, 10101010, 20202020, 30303030, 40404040, 50505050, 60606060, 70707070, 80808080, 90909090],
         population_size=[1000],
         elitism_ratio=[0.2],
         elitism_size=[None],
